33a28dfff8c9586d57595db3b2702213609b2d1e
tokenfox <tokenfox@protonmail.com>
2024-03-13 23:28:30 +0200

    add: petarp images

--- petarp-images.sh
+++ petarp-images.sh
@@ -4,7 +4,7 @@
 for i in {0..63}
 do
    wget -r -p -E -np -N "https://nopsta.com/petarp/animations/${i}"
+   wget -r -p -E -np -N "https://nopsta.com/petarp/images/${i}.png"
    padded_number=$(printf "%03d" $i)
    wget -r -p -E -np -N "https://nopsta.com/petarp/images/petarp-${padded_number}-bordered.gif"
-
 done
