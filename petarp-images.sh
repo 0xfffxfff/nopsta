3ef88a4b7a8a43ba5793b3a4e16d062a1ac11613
tokenfox <tokenfox@protonmail.com>
2024-03-13 23:37:54 +0200

    chore: add more petarp images

--- petarp-images.sh
+++ petarp-images.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
 for i in {0..63}
 do
    wget -r -p -E -np -N "https://nopsta.com/petarp/animations/${i}"
    wget -r -p -E -np -N "https://nopsta.com/petarp/images/${i}.png"
    padded_number=$(printf "%03d" $i)
    wget -r -p -E -np -N "https://nopsta.com/petarp/images/petarp-${padded_number}-bordered.gif"
+   wget -r -p -E -np -N "https://nopsta.com/petarp/images/petarp-${padded_number}.gif"
 done
