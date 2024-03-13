c5a3faf93677fc9bb744fe91fb2b9c973fec1034
0xfff <0xfff@protonmail.com>
2024-03-13 17:51:29 +0100

    feat: add mockup endpoints / resolve specific token urls with index.html

--- index.js
+++ index.js
@@ -4,6 +4,33 @@ const app = express();
 
 app.use(express.static("nopsta.com", { extensions: ["html"] }));
 
+app.use(`/oraand/:id`, (_, res) =>
+  res.sendFile(`nopsta.com/oraand/index.html`, { root: __dirname })
+);
+app.use(`/petarp/:id`, (_, res) =>
+  res.sendFile(`nopsta.com/petarp/index.html`, { root: __dirname })
+);
+
+app.get("/a/oraand/token-count", (_, res) => res.json({ value: 1024 }));
+app.get("/a/oraand/mint-price", (_, res) => res.json({ value: 0.0256 }));
+
+app.get("/a/petarp/token-count", (_, res) => res.json({ value: 64 }));
+app.get("/a/petarp/mint-price", (_, res) => res.json({ value: 2560 }));
+app.get("/a/petarp/versions", (_, res) =>
+  // 0xfff: TODO: Find out if this is derived from chainstate
+  res.json({
+    value: [
+      1, 0, 0, 1, 1, 2, 1, 1, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    ],
+  })
+);
+
+app.get("/a/petarp/custom-bytes", (_, res) =>
+  res.json(require("./mock").CUSTOM_BYTES_RESPONSE_STUB)
+);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
